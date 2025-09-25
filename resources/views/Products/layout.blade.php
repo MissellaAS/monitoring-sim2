82ad262a6c3884669a8532466161fcb6e5cc5d92
sinagarafhael <sinagarafhael@gmail.com>
2025-09-25 16:10:22 +0700

    command index

--- resources/views/Products/layout.blade.php
+++ resources/views/Products/layout.blade.php
@@ -3,8 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <title>Vesta Manufacturing</title>
-  <style>
+  {{-- <style>
     body, html {
       margin: 0;
       padding: 0;
@@ -66,7 +67,7 @@
       line-height: 1.5;
       letter-spacing: 1px;
     }
-  </style>
+  </style> --}}
 </head>
 <body>
   <!-- Header -->
@@ -85,27 +86,6 @@
         OF INDUSTRIAL MACHINE PARTS
       </p>
     </div>
-<<<<<<< HEAD
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"  integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script>
-        document.getElementByid('sidebarToggle').addEventListener('click', function () {
-            document.getElementById('sidebar').classList.toggle('collapsed');
-        });
-    </script>
-    
-    <div class="Izwocg" style="width: 100%; height: 95%; overflow: hidden;">
-  <img 
-    class="_7_i_XA" 
-    src="https://media-public.canva.com/gvGO0/MAEYOOgvGO0/1/s.jpg" 
-    alt="Modern building facade of financial company"
-    style="width: 100%; height: 95%; object-fit: cover;"
-  >
-</div>
-
-    <div class="bFnJ2A _7vS1Yw _682gpw" style="clip-path: url(&quot;#__id146&quot;); background: rgb(0, 0, 0); width: 256px; height: 621.596px; transform: scale(1.23553, 1.23553); transform-origin: 0px 0px; touch-action: pan-x pan-y pinch-zoom;"></div>
-
-=======
   </div>
->>>>>>> 9cabd5499c1b16414ef49513a95e7ac1de7212b6
 </body>
 </html>
