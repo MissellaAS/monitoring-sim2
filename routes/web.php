c87b2ebfd90bf4cde445d8a78d43856799a0e88a
yeniirahmaa512 <yenirahma396@gmail.com>
2025-09-25 11:57:57 +0700

    bebas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/product-customer', function () {
+    $products = Product::all(); // ambil semua data product dari database
+    return view('product', compact('products'));
 });
