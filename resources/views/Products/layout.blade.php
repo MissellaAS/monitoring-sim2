e55752cc7bee0d48751907d95ee1329ff15f418d
unknown <renatavero564@gmail.com>
2025-09-25 13:59:15 +0700

    tatag/4 Folder public

--- resources/views/Products/layout.blade.php
+++ resources/views/Products/layout.blade.php
@@ -3,77 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ assets('assets/css/style.css')}}">
   <title>Vesta Manufacturing</title>
-  <style>
-    body, html {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      font-family: Arial, sans-serif;
-    }
 
-    /* Header hitam */
-    .navbar {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 50px;
-      background-color: black;
-      display: flex;
-      align-items: center;
-      padding: 0 20px;
-      z-index: 2;
-    }
-
-    .menu-icon {
-      font-size: 24px;
-      color: white;
-      cursor: pointer;
-    }
-
-    /* Background gedung */
-    .hero {
-      height: 100vh;
-      background: url("https://media-public.canva.com/gvGO0/MAEYOOgvGO0/1/s.jpg") no-repeat center center;
-      background-size: cover;
-      position: relative;
-      color: white;
-    }
-
-    /* Teks di kanan */
-    .hero-text {
-      position: absolute;
-      top: 30%;
-      right: 5%;
-      text-align: right;
-    }
-
-    .hero-text h1 {
-      font-size: 50px;
-      margin: 0;
-    }
-
-    .hero-text h2 {
-      font-size: 30px;
-      margin: 0;
-      font-weight: bold;
-    }
-
-    .hero-text p {
-      font-size: 14px;
-      margin-top: 10px;
-      line-height: 1.5;
-      letter-spacing: 1px;
-    }
-  </style>
 </head>
 <body>
   <!-- Header -->
   <div class="navbar">
     <div class="menu-icon">&#9776;</div> <!-- ikon menu -->
   </div>
-
+  @yield('content')
   <!-- Hero section -->
   <div class="hero">
     <div class="hero-text">
