e677f8722e5689b61e67f1cc2940f7041d9c272f
RenV564 <renatavero564@gmail.com>
2025-10-01 15:07:53 +0700

    hgfdsqa

--- resources/views/orders/layout.blade.php
+++ resources/views/orders/layout.blade.php
@@ -17,7 +17,7 @@
   <div class="nav-links" id="menu">
     <a href="#">Home</a>
     <a href="{{ route('orders.index') }}">Order List</a>
-    <a href="#">Production Monitoring</a>
+    <a href="{{ route('products.create') }}">Production Monitoring</a>
     <a href="{{ route('machines.index') }}">Machine Monitoring</a>
   </div>
 
